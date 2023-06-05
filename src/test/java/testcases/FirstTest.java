b3dcadcdffe1a09e07fddde3c4f408b3deb97998
ViktoriiaKucherenko <135615721+ViktoriiaKucherenko@users.noreply.github.com>
2023-06-05 08:24:06 -0400

    First commit

---
+++ src/test/java/testcases/FirstTest.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void openBrowser(){
+        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://mvnrepository.com/");
+        driver.manage().window().maximize();
+    }
+}

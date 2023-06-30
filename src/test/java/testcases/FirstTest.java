2f278df8416f274472eae9461be4b5fca9353894
ViktoriiaKucherenko <135615721+ViktoriiaKucherenko@users.noreply.github.com>
2023-06-30 08:16:31 -0400

    java_lesson7

--- src/test/java/testcases/FirstTest.java
+++ src/test/java/testcases/FirstTest.java
@@ -3,73 +3,84 @@ package testcases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 
+    public ChromeDriver driver;
+    public String websiteURL = "https://shafa.ua/";
+    public By UserSignInButton = By.xpath("//a[contains(text(),'Вхід')]");
+    public By EmailField = By.xpath("//input[@name='username']");
+    public By PasswordField = By.xpath("//input[@name='password']");
+    public By SubmitSigningInButton = By.xpath("//button[contains(text(),'Увійти')]");
+    public String UserEmail = "TestEmail";
+    public String UserPassword = "pwd";
+    public By UserValidationError = By.xpath("//div[contains(text(),\"Будь ласка, введіть правильні ім'я користувача та \")]");
+
+    @BeforeMethod
+    public void ChromeSetUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+    @AfterMethod
+    public void ChromeClose(){
+        driver.close();
+    }
+
+
     @Test
     public void OpenWebsite() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://test.my-fork.com/");
+        driver.get(websiteURL);
         driver.manage().window().maximize();
+        Thread.sleep(3000);
 
     }@Test
     public void OpenSignInPage() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://test.my-fork.com/");
+        driver.get(websiteURL);
         driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//div[@id='log-in-button']")).click();
+        driver.findElement(UserSignInButton).click();
+        Thread.sleep(3000);
     }@Test
     public void ValidateFieldsAndButtonDisplaying() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://test.my-fork.com/");
+        driver.get(websiteURL);
         driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//div[@id='log-in-button']")).click();
-        System.out.println(driver.findElement(By.xpath("//input[@id='email']")).isDisplayed());
-        System.out.println(driver.findElement(By.xpath("//input[@id='password']")).isDisplayed());
-        System.out.println(driver.findElement(By.xpath("//button[@type='submit']")).isDisplayed());
+        driver.findElement(UserSignInButton).click();
+
+        System.out.println(driver.findElement(EmailField).isDisplayed());
+        System.out.println(driver.findElement(PasswordField).isDisplayed());
+        System.out.println(driver.findElement(SubmitSigningInButton).isDisplayed());
+        Thread.sleep(3000);
     }@Test
     public void EnterIncorrectData() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://test.my-fork.com/");
+        driver.get(websiteURL);
         driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//div[@id='log-in-button']")).click();
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("testEmail");
+        driver.findElement(UserSignInButton).click();
+
+        driver.findElement(EmailField).sendKeys(UserEmail);
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("pwd");
+        driver.findElement(PasswordField).sendKeys(UserPassword);
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(SubmitSigningInButton).click();
+        Thread.sleep(4000);
     }@Test
     public void IncorrectEmailError() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://test.my-fork.com/");
+        driver.get(websiteURL);
         driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//div[@id='log-in-button']")).click();
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("testEmail");
+        driver.findElement(UserSignInButton).click();
+
+        driver.findElement(EmailField).sendKeys(UserEmail);
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("pwd");
+        driver.findElement(PasswordField).sendKeys(UserPassword);
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        driver.findElement(SubmitSigningInButton).click();
         Thread.sleep(5000);
-        //p[contains(text(),'Error: email is incorrect')]
-        System.out.println(driver.findElement(By.xpath("//p[contains(text(),'Error: email is incorrect')]")).isDisplayed());
-    }@Test
-    public void ValidateCheckbox() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\ProjectAutomation\\TestProject\\src\\test\\resources\\executables\\chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://test.my-fork.com/");
-        driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//div[@id='log-in-button']")).click();
-        System.out.println(driver.findElement(By.id("auth-page-remember-me")).isSelected());
+        System.out.println(driver.findElement(UserValidationError).isDisplayed());
     }
 }
